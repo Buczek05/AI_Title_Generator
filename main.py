fcca0b991bf1f864029db2c761d5150ede3ce104
Buczek05 <kacperbuko2005@gmail.com>
2023-08-27 14:04:23 +0200

    code refactoring

--- main.py
+++ main.py
@@ -12,158 +12,287 @@ from PyQt6.QtWidgets import (
     QHBoxLayout,
     QLabel,
 )
+import shutil
 from PyQt6.QtGui import QIcon
 
-SETTINGS_PATH = os.path.expanduser("~") + "/Documents/AI_Title_Generator/"
+SETTINGS_PATH = os.path.expanduser("~") + "/.AI_Title_Generator/"
 SETTINGS_FILE = SETTINGS_PATH + "settings.json"
-
-
-def save_settings(api_key: str, input_file: str, output_folder: str) -> None:
-    """Save settings to json file"""
-    if not os.path.exists(SETTINGS_PATH):
-        os.makedirs(SETTINGS_PATH)
-    json.dump(
-        {"api_key": api_key, "input_file": input_file, "output_folder": output_folder},
-        open(SETTINGS_FILE, "w"),
-    )
-    print(SETTINGS_FILE)
-
-
-def get_settings() -> dict:
-    """Get settings from json file"""
-    try:
-        return json.load(open(SETTINGS_FILE, "r"))
-    except FileNotFoundError:
-        return {}
+GPT_SYSTEM_MESSAGE = "Create short (max 10 words) title for program based on it code, in tytle describe what this program do. Return only title."
+GTP_SYSTEM_MESSAGE_DIR = {"role": "system", "content": GPT_SYSTEM_MESSAGE}
 
 
 class Window(QDialog):
-    def __init__(self):
-        super().__init__()
+    def setup_window(self):
+        self.main_layout = QVBoxLayout()
         self.setWindowTitle("Title Generator")
         self.resize(600, 100)
+        self.setLayout(self.main_layout)
 
+    def setup_layout_line(self, *widgets):
+        line_layout = QHBoxLayout()
+        for widget in widgets:
+            line_layout.addWidget(widget)
+        self.main_layout.addLayout(line_layout)
+
+    def setup_layout(self):
+        self.setup_api_line()
+        self.setup_path()
+        self.setup_output_folder()
+        self.setup_generate_button()
+
+    # API KEY
+    def setup_view_api_line(self):
         self.api_key_label = QLabel("API key:")
-        self.api_key_edit = QLineEdit()
-        self.api_key_edit.setEchoMode(QLineEdit.EchoMode.Password)
+        self.api_key_line_edit = QLineEdit()
         self.show_api_key_button = QToolButton()
-        self.show_api_key_button.setText("Show")
+
+    def setup_property_api_line(self):
+        self.api_key_line_edit.textChanged.connect(self.changed_open_ai_api_key)
         self.show_api_key_button.setCheckable(True)
-        self.show_api_key_button.toggled.connect(self.show_hide_api_key)
-        self.lyt_api_key = QHBoxLayout()
-        self.lyt_api_key.addWidget(self.api_key_label)
-        self.lyt_api_key.addWidget(self.api_key_edit)
-        self.lyt_api_key.addWidget(self.show_api_key_button)
-        self.api_key_edit.textChanged.connect(self.setup_api_key)
+        self.show_api_key_button.toggled.connect(self.toggle_api_key_line_edit_show)
 
-        self.path_label = QLabel("Input file:")
-        self.path_edit = QLineEdit()
-        self.choose_path_button = QToolButton()
-        self.choose_path_button.setText("...")
-        self.choose_path_button.clicked.connect(self.choose_path)
-        self.lyt_path = QHBoxLayout()
-        self.lyt_path.addWidget(self.path_label)
-        self.lyt_path.addWidget(self.path_edit)
-        self.lyt_path.addWidget(self.choose_path_button)
+        self.hide_text_api_key_line_edit()
 
-        self.output_folder_label = QLabel("Output folder:")
-        self.output_folder_edit = QLineEdit()
-        self.choose_output_folder_button = QToolButton()
-        self.choose_output_folder_button.setText("...")
-        self.choose_output_folder_button.clicked.connect(self.choose_output_folder)
-        self.lyt_output_folder = QHBoxLayout()
-        self.lyt_output_folder.addWidget(self.output_folder_label)
-        self.lyt_output_folder.addWidget(self.output_folder_edit)
-        self.lyt_output_folder.addWidget(self.choose_output_folder_button)
+    def setup_api_line(self):
+        self.setup_view_api_line()
+        self.setup_property_api_line()
+        self.setup_layout_line(
+            self.api_key_label, self.api_key_line_edit, self.show_api_key_button
+        )
 
-        self.generate_button = QPushButton("Generate")
-        self.generate_button.clicked.connect(self.generate)
+    def show_text_api_key_line_edit(self):
+        self.api_key_line_edit.setEchoMode(QLineEdit.EchoMode.Normal)
+        self.show_api_key_button.setText("Hide")
 
-        self.lyt = QVBoxLayout()
-        self.lyt.addLayout(self.lyt_api_key)
-        self.lyt.addLayout(self.lyt_path)
-        self.lyt.addLayout(self.lyt_output_folder)
-        self.lyt.addWidget(self.generate_button)
-        self.setLayout(self.lyt)
+    def hide_text_api_key_line_edit(self):
+        self.api_key_line_edit.setEchoMode(QLineEdit.EchoMode.Password)
+        self.show_api_key_button.setText("Show")
 
-        settings = get_settings()
+    def toggle_api_key_line_edit_show(self):
+        if self.show_api_key_button.isChecked():
+            self.show_text_api_key_line_edit()
+        else:
+            self.hide_text_api_key_line_edit()
+
+    def changed_open_ai_api_key(self):
         try:
-            if settings:
-                self.api_key_edit.setText(settings["api_key"])
-                openai.api_key = settings["api_key"]
-                self.path_edit.setText(settings["input_file"])
-                self.output_folder_edit.setText(settings["output_folder"])
-        except KeyError:
-            pass
-
-    def setup_api_key(self):
-        """Setup api key for openai"""
-        try:
-            openai.api_key = self.api_key_edit.text()
+            openai.api_key = self.api_key_line_edit.text()
         except Exception as e:
             QMessageBox.critical(self, "Error", str(e))
 
-    def show_hide_api_key(self):
-        """Switch between showing and hiding api key by changing echo mode of QLineEdit"""
-        if self.show_api_key_button.isChecked():
-            self.api_key_edit.setEchoMode(QLineEdit.EchoMode.Normal)
-            self.show_api_key_button.setText("Hide")
-        else:
-            self.api_key_edit.setEchoMode(QLineEdit.EchoMode.Password)
+    # MAIN FILE PATH
+    def setup_view_path(self):
+        self.file_path_label = QLabel("Input file:")
+        self.main_file_path = QLineEdit()
+        self.choose_file_path_button = QToolButton()
 
-    def choose_path(self):
-        """Choose input file by opening file dialog"""
-        path = QFileDialog.getOpenFileName(self, "Choose input file")
-        self.path_edit.setText(path[0])
+    def setup_property_path(self):
+        self.choose_file_path_button.setText("...")
+        self.choose_file_path_button.clicked.connect(self.choose_file_path)
+
+    def setup_path(self):
+        self.setup_view_path()
+        self.setup_property_path()
+        self.setup_layout_line(
+            self.file_path_label, self.main_file_path, self.choose_file_path_button
+        )
+
+    def choose_file_path(self):
+        file_path = QFileDialog.getOpenFileName(self, "Choose input file")
+        self.main_file_path.setText(file_path[0])
+
+    # OUTPUT FOLDER
+    def setup_view_output_folder(self):
+        self.output_folder_label = QLabel("Output folder:")
+        self.output_folder_edit = QLineEdit()
+        self.choose_output_folder_button = QToolButton()
+
+    def setup_property_output_folder(self):
+        self.choose_output_folder_button.setText("...")
+        self.choose_output_folder_button.clicked.connect(self.choose_output_folder)
+
+    def setup_output_folder(self):
+        self.setup_view_output_folder()
+        self.setup_property_output_folder()
+        self.setup_layout_line(
+            self.output_folder_label,
+            self.output_folder_edit,
+            self.choose_output_folder_button,
+        )
 
     def choose_output_folder(self):
-        """Choose output folder by opening file dialog"""
         path = QFileDialog.getExistingDirectory(self, "Choose output folder")
         self.output_folder_edit.setText(path)
 
-    def generate(self):
-        """Use GPT-3 to generate title for program based on it code"""
+    # GENERATE BUTTON
+    def setup_view_generate_button(self):
+        self.generate_button = QPushButton("Generate")
+
+    def setup_property_generate_button(self):
+        self.generate_button.clicked.connect(self.generate)
+
+    def setup_generate_button(self):
+        self.setup_view_generate_button()
+        self.setup_property_generate_button()
+        self.main_layout.addWidget(self.generate_button)
+
+    # SETTINGS
+    def check_settings(self, settings_dict: dict) -> bool:
+        if not settings_dict:
+            return False
+
+        for key in settings_dict:
+            if not settings_dict[key]:
+                return False
+
+        return True
+
+    def apply_settings(self, settings_dict: dict):
+        self.api_key_line_edit.setText(settings_dict["api_key"])
+        openai.api_key = settings_dict["api_key"]
+        self.main_file_path.setText(settings_dict["input_file"])
+        self.output_folder_edit.setText(settings_dict["output_folder"])
+
+    def load_settings(self):
+        settings_dict = self.load_user_settings()
+        if self.check_settings(settings_dict):
+            self.apply_settings(settings_dict)
+
+    def __init__(self):
+        super().__init__()
+
+        self.setup_window()
+        self.setup_layout()
+        self.load_settings()
+
+    def check_if_file_exist(self, path: str) -> bool:
+        return os.path.exists(path)
+
+    def check_if_file_is_text_file(self, path: str) -> bool:
         try:
-            with open(self.path_edit.text(), "r") as f:
-                text = f.read()
-        except FileNotFoundError:
-            QMessageBox.critical(self, "Error", "File not found")
-            return
+            with open(path, "r") as f:
+                f.read()
+            return True
         except UnicodeDecodeError:
-            QMessageBox.critical(self, "Error", "File is not a text file")
-            return
+            return False
 
-        message_to_GPT = "Create short (max 10 words) title for program based on it code, in tytle describe what this program do. Return only title."
+    def check_if_output_folder_exist(self, path: str) -> bool:
+        return os.path.exists(path)
 
-        response = openai.ChatCompletion.create(
+    def check_file_path(self, file_path) -> bool:
+        if not self.check_if_file_exist(file_path):
+            QMessageBox.critical(self, "Error", "File not found")
+            return False
+        if not self.check_if_file_is_text_file(file_path):
+            QMessageBox.critical(self, "Error", "File is not text file")
+            return False
+        return True
+
+    def check_output_folder(self, output_folder) -> bool:
+        if not self.check_if_output_folder_exist(output_folder):
+            QMessageBox.critical(self, "Error", "Output folder not found")
+            return False
+        return True
+
+    def setup_file_path_and_output_folder_if_everything_is_ok(self) -> bool:
+        self.file_path = self.main_file_path.text()
+        self.output_folder = self.output_folder_edit.text()
+
+        if not self.check_file_path(self.file_path) or not self.check_output_folder(
+            self.output_folder
+        ):
+            return False
+        return True
+
+    def get_file_extension(self, file_path: str) -> str:
+        return file_path.split(".")[-1]
+
+    def get_file_content(self, file_path: str) -> str:
+        with open(file_path, "r") as f:
+            return f.read()
+
+    def get_messages_to_ai(self, file_content: str) -> list:
+        gpt_user_message = {"role": "user", "content": file_content}
+        return [GTP_SYSTEM_MESSAGE_DIR, gpt_user_message]
+
+    def get_response_from_ai(self, messages) -> dict:
+        return openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": message_to_GPT},
-                {"role": "user", "content": text},
-            ],
+            messages=messages,
         )
-        self.file_title = response["choices"][0]["message"]["content"]
-        input_file_extension = self.path_edit.text().split(".")[-1]
-        self.output_file_path = (
-            self.output_folder_edit.text()
+
+    def generate_file_title(self) -> None:
+        file_content = self.get_file_content(self.file_path)
+        messages_to_ai = self.get_messages_to_ai(file_content)
+        response_from_ai = self.get_response_from_ai(messages_to_ai)
+        self.file_title_generated_by_ai = response_from_ai["choices"][0]["message"][
+            "content"
+        ]
+
+    def get_output_file_path(self) -> str:
+        file_extension = self.get_file_extension(self.file_path)
+        output_file_path = (
+            self.output_folder
             + "/"
-            + self.file_title
+            + self.file_title_generated_by_ai
             + "."
-            + input_file_extension
+            + file_extension
         )
-        with open(self.output_file_path, "w") as f:
-            f.write(text)
+        return output_file_path
+
+    def copy_file(self) -> bool:
+        try:
+            shutil.copyfile(self.file_path, self.output_file_path)
+            return True
+        except Exception as e:
+            QMessageBox.critical(self, "Error", str(e))
+            return False
+
+    def generate(self):
+        if not self.setup_file_path_and_output_folder_if_everything_is_ok():
+            return
+        self.generate_file_title()
+        self.output_file_path = self.get_output_file_path()
+        if not self.copy_file():
+            return
         QMessageBox.information(
             self, "Success", "File saved as " + self.output_file_path
         )
 
-    def closeEvent(self, event) -> None:
-        """Save settings on close"""
-        save_settings(
-            self.api_key_edit.text(),
-            self.path_edit.text(),
-            self.output_folder_edit.text(),
+    def create_dir_if_not_exists(self, path: str) -> None:
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    def create_user_settings_dict(self) -> None:
+        user_settings_dict = {
+            "api_key": self.api_key_line_edit.text(),
+            "input_file": self.main_file_path.text(),
+            "output_folder": self.output_folder_edit.text(),
+        }
+        return user_settings_dict
+
+    def save_user_settings_to_file(self, user_settings_dict: dict) -> None:
+        json.dump(
+            user_settings_dict,
+            open(SETTINGS_FILE, "w"),
         )
+
+    def save_user_settings(self) -> None:
+        self.create_dir_if_not_exists(SETTINGS_PATH)
+        user_settings_dict = self.create_user_settings_dict()
+        self.save_user_settings_to_file(user_settings_dict)
+
+    def load_user_settings(self) -> dict:
+        try:
+            user_settings_dict = json.load(open(SETTINGS_FILE, "r"))
+
+        except FileNotFoundError:
+            user_settings_dict = {}
+
+        return user_settings_dict
+
+    def closeEvent(self, event) -> None:
+        self.save_user_settings()
         return super().closeEvent(event)
 
 
